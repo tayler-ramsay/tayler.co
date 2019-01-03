ebd6c233cf74a0e5e794831dc2936517160a3896
Tayler Ramsay <ramsay.tayler@gmail.com>
2019-01-03 11:05:04 -0500

    inital

---
+++ components/TheNavBar.vue
@@ -0,0 +1,22 @@
+<template>
+    <nav class="nav">
+        <div class="logo">
+            <nuxt-link :to="{name: 'index'}" class="logo text-lg">
+                Tayler's Fundamentals
+            </nuxt-link>
+            <span class="subheader">
+                A Vue App in Nuxt
+            </span>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <section class="container">
+    <div>
+      <logo/>
+      <h1 class="title">nuxt-funds</h1>
+      <h2 class="subtitle">My premium Nuxt.js project</h2>
+      <div class="links">
+        <nuxt-link
+          v-for="post in posts"
+          :to="{name: 'posts-id', params: {id: post.id}}"
+          key="post.id"
+          class="button--grey"
+        >{{post.title}}</nuxt-link>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+
+export default {
+  components: {
+    Logo
+  },
+  head() {
+    return {
+      title: "Home Page"
+    };
+  },
+  computed: {
+    posts() {
+      return this.$store.state.posts.all;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
