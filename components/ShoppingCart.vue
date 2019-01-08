7e359246f920b8d175f149e6b5bb4104a3b5c64c
Tayler Ramsay <ramsay.tayler@gmail.com>
2019-01-08 09:39:40 -0500

    update cart

---
+++ components/ShoppingCart.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Shopping Cart</h1>
+    <ul>
+      <li
+        v-for="product in products"
+        :key="product.id"
+      >{{product.title}} - {{product.price}}- {{product.quantity}}</li>
+    </ul>
+    <p>Total: {{total | currency}}</p>
+    <button @click="this.checkout">Checkout</button>
+    <p v-if="checkoutStatus">{{checkoutStatus}}</p>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters, mapActions } from "vuex";
+export default {
+  computed: {
+    ...mapGetters({
+      products: "cart/cartProducts",
+      total: "cart/cartTotal"
+    }),
+    ...mapState({
+      checkoutStatus: state => state.cart.checkoutStatus
+    })
+  },
+  methods: {
+      ...mapActions({
+          checkout:'cart/checkout'
+      })
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ pages/cart.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ProductList/>
+    <hr/>
+    <ShoppingCart/>
+  </div>
+</template>
+
+ <script>
+import ProductList from "@/components/ProductList";
+import ShoppingCart from "@/components/ShoppingCart";
+
+export default {
+  layout: 'shoppingCart',
+  components: {
+    ProductList,
+    ShoppingCart
+  }
+};
+</script>
+
+</style>
+<style scoped>
