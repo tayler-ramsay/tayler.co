0711887fd93f52d18a3bc5c6a12900fc0b742c53
Tayler Ramsay <ramsay.tayler@gmail.com>
2019-08-25 16:47:09 -0400

    updates

--- components/TheNavBar.vue
+++ components/TheNavBar.vue
@@ -2,23 +2,14 @@
   <nav class="nav">
     <div class="logo">
       <nuxt-link :to="{name: 'index'}" class="logo text-lg">Tayler Ramsay</nuxt-link>
-<<<<<<< HEAD
-      <span>I am a frontend developer working at
-        <a href="https://Pavone.net" target="blank">Pavone Marketing</a>
-=======
       <span>
         I am a frontend developer working for
         <a href="#">Versatile Credit</a>
->>>>>>> master
       </span>
     </div>
     <div>
       <nuxt-link :to="{name: 'cart'}" class="text-lg">Cart</nuxt-link>
-<<<<<<< HEAD
-    </div>
-=======
     </div>-->
->>>>>>> master
   </nav>
 </template>
 
