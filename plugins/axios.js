dd3ca52509d5ee177ef3e38a2e85453fc35de361
Tayler Ramsay <ramsay.tayler@gmail.com>
2019-08-25 17:17:18 -0400

    remove dev

--- plugins/axios.js
+++
@@ -1,6 +0,0 @@
-export default function({$axios}) {
-    $axios.setHeader('AUTHO', 'passing bearertoken')
-    $axios.onRequest(config => {
-        console.log('---------------Making request to ' + config.url)
-    })
-}
